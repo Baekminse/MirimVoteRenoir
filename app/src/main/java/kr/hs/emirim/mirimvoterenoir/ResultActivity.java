8641b9a5f4027c6ab9f8bbde2304f9480d8116a0
dwrdd <s2019w30@e-mirim.hs.kr>
2020-11-24 20:28:08 +0900

    20201124

--- app/src/main/java/kr/hs/emirim/mirimvoterenoir/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/mirimvoterenoir/ResultActivity.java
@@ -25,6 +25,8 @@ public class ResultActivity extends AppCompatActivity {
         int[] textIds = {R.id.text1,R.id.text2, R.id.text3,R.id.text4,R.id.text5,R.id.text6,R.id.text7,R.id.text8,R.id.text9};
         int[] ratingIds = {R.id.rating_bar1,R.id.rating_bar2,R.id.rating_bar3,R.id.rating_bar4,R.id.rating_bar5,R.id.rating_bar6,R.id.rating_bar7,R.id.rating_bar8,R.id.rating_bar9};
         int temp = 0;
+        Integer imageFiledId[] = {R.drawable.jae1, R.drawable.jae2, R.drawable.jae3, R.drawable.jae4, R.drawable.jae5,
+                R.drawable.jae6, R.drawable.jae7, R.drawable.jae8, R.drawable.jae9};
         for(int i = 0; i<imgNames.length; i++) {
             textVs[i] = findViewById(textIds[i]);
             ratingBars[i] = findViewById(ratingIds[i]);
